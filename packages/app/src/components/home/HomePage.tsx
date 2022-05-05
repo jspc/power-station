2f27d569e77fb3b8ff17ad109b2f498415cc34e6
James Condron <james.condron+github@kaluza.com>
2022-05-05 10:57:28 +0100

    Include background music

--- packages/app/src/components/home/HomePage.tsx
+++ packages/app/src/components/home/HomePage.tsx
@@ -5,27 +5,29 @@ import styles from './HomePage.module.css';
 
 export const HomePage = () => {
     return (
-<Grid
-  container
-  spacing={0}
-  direction="column"
-  alignItems="center"
-  justifyContent="center"
-        className={styles.background}
->
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            className={styles.background}
+        >
 
-          <Grid item xs={0} md={4}>
-          </Grid>
+            <Grid item xs={false} md={4}>
+            </Grid>
 
-          <Grid item xs={12} md={4} className={styles.card}>
-          <h1>
-          Power Station
-      </h1>
+            <Grid item xs={12} md={4} className={styles.card}>
+                <h1>
+                    Power Station
+                </h1>
+            </Grid>
 
-          <h2>
-          gon give it to ya
-          </h2>
-          </Grid>
-      </Grid>
-  );
+            <audio autoPlay>
+                <source src="https://upload.wikimedia.org/wikipedia/commons/3/32/Mega_Hyper_Ultrastorm_%28ISRC_USUAN1700003%29.mp3" type="audio/mpeg" />
+                    Your browser does not support the audio element.
+            </audio>
+
+        </Grid>
+    );
 };
